98b6ec14d402669b688c3b59fb111f0bf5036c6b
Mena <98476765+m3na02@users.noreply.github.com>
2024-02-05 13:56:02 +0200

    Implement CalorieScreen with weight input functionality

--- app/src/main/java/com/example/calorie/MainActivity.kt
+++ app/src/main/java/com/example/calorie/MainActivity.kt
@@ -8,12 +8,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,18 +44,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun CalorieScreen(){
-    Column(
-        modifier = Modifier.padding(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
-        Heading(title = stringResource(R.string.title))
-    }
-
-}
-
 @Composable
 fun Heading(title: String){
     Text(
@@ -59,6 +55,33 @@ fun Heading(title: String){
             .fillMaxWidth()
             .padding(top = 16.dp, bottom = 16.dp)
     )
+
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WeightField(weightInput: String, onValueChange:(String) -> Unit) {
+    OutlinedTextField(
+        value = weightInput,
+        onValueChange = onValueChange,
+        label = {Text(text = "Enter Weight")},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp, bottom = 16.dp)
+    )
+}
+
+@Composable
+fun CalorieScreen(){
+    var weightInput by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Heading(title = stringResource(R.string.title))
+        WeightField(weightInput = weightInput, onValueChange = { weightInput = it })
+    }
 
 }
 
