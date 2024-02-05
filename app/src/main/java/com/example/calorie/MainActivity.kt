d8b64fb519fed8485a76a320a24f305842d985ef
Mena <98476765+m3na02@users.noreply.github.com>
2024-02-05 13:33:17 +0200

    Create a new composable componet under the name Heading and adding a coloumn to CalorieScreen

--- app/src/main/java/com/example/calorie/MainActivity.kt
+++ app/src/main/java/com/example/calorie/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.calorie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calorie.ui.theme.CalorieTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CalorieScreen(){
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Heading(title = stringResource(R.string.title))
+    }
+
+}
+
+@Composable
+fun Heading(title: String){
+    Text(
+        text = title,
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp, bottom = 16.dp)
+    )
 
 }
 
